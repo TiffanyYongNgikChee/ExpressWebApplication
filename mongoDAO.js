4a2672c31c5be4a48e4ff4fc8166a7301d9852fc
TiffanyYongNgikChee <G00425067@atu.ie>
2024-12-19 09:59:38 +0000

    Lecture page- function to find all lecturers sorted by lecturer ID

--- mongoDAO.js
+++ mongoDAO.js
@@ -10,20 +10,20 @@ MongoClient.connect('mongodb://127.0.0.1:27017')
     console.log(error.message)
     })
 
-// Function to find all documents
-function findAll() {
+// Function to find all lecturers sorted by lecturer ID
+function findAllSorted() {
     return new Promise((resolve, reject) => {
         if (!coll) {
             return reject(new Error('Collection not initialized'));
         }
-        coll.find()
-            .toArray()
+        coll.find().sort({ lid: 1 }).toArray()
             .then((documents) => resolve(documents))
             .catch((error) => reject(error));
     });
 }
 
+
 // Export the findAll function
 module.exports = {
-    findAll,
+    findAllSorted,
 };
